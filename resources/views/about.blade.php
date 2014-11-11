6c1181dab594468f7166c57221a6147a808411f2
Kristoffer Hogberg <krihog@gmail.com>
2014-11-11 14:50:46 +0100

    Update about copy

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,7 +9,7 @@
                 Detta är lmks inofficiella stegräknartävling.
                 Den använder sig av Google Fit för att spåra stegen.
             </p>
-            <p>Finns tillgänglig för:</p>
+            <p>Google Fit finns tillgänglig för:</p>
             <ul>
                 <li><a href="https://play.google.com/store/apps/details?id=com.google.android.apps.fitness">Android (inkl Wear)</a> (måste installeras på telefonen för att synka mot Googles server)</li>
                 <li><a href="https://fit.google.com/">Web (ingen stegräkning)</a></li>
