68632ad8f0594b103ed0fda9ad0d38116aa2378a
Kristoffer Högberg <krihog@gmail.com>
2015-09-21 22:05:04 +0200

    Update middleware with scrutinizer improvements and PSR-2

--- app/Http/Middleware/AuthenticatedWithBasicAuth.php
+++ app/Http/Middleware/AuthenticatedWithBasicAuth.php
@@ -2,38 +2,36 @@
 
 use Closure;
 use Illuminate\Contracts\Auth\Guard;
-use Illuminate\Contracts\Routing\Middleware;
 
-class AuthenticatedWithBasicAuth implements Middleware {
+class AuthenticatedWithBasicAuth
+{
 
-	/**
-	 * The Guard implementation.
-	 *
-	 * @var Guard
-	 */
-	protected $auth;
+    /**
+     * The Guard implementation.
+     *
+     * @var Guard
+     */
+    protected $auth;
 
-	/**
-	 * Create a new filter instance.
-	 *
-	 * @param  Guard  $auth
-	 * @return void
-	 */
-	public function __construct(Guard $auth)
-	{
-		$this->auth = $auth;
-	}
+    /**
+     * Create a new filter instance.
+     *
+     * @param  Guard $auth
+     */
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
 
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Closure  $next
-	 * @return mixed
-	 */
-	public function handle($request, Closure $next)
-	{
-		return $this->auth->basic() ?: $next($request);
-	}
-
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $this->auth->basic() ?: $next($request);
+    }
 }
